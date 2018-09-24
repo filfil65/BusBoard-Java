0e77c8e4123f7dabe3cc37db9c527c790aaeb167
BROWN Samuel <sabrown@europe.corp.altran.com>
2018-09-24 11:53:25 +0100

    response get

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,18 +1,41 @@
 package training.busboard;
 
+import java.util.Scanner;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
     	// Sam's API ID is: c3b16dc7
     	// Sam's API key is : 5293d7854df1ddc814d674a109060164
+    	String apiid = "c3b16dc7";
+    	String apikey = "5293d7854df1ddc814d674a109060164";
     	
+    	Scanner readline = new Scanner(System.in);  
     	System.out.println("Please enter an ATCO code for a bus stop:");
-		String input = readline.nextLine().toLowerCase(); 
+		String input = readline.nextLine(); 
+		readline.close();
     	
     	Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-    	String name = client.target("https://transportapi.com/v3/uk/bus/stop/" + string +"/live.json?app_id=c3b16dc7&app_key=5293d7854df1ddc814d674a109060164&group=route&nextbuses=no")
-    	        .request(MediaType.TEXT_PLAIN)
-    	        .get(String.class);
+//    	String schedule = client.target("https://transportapi.com/v3/uk/bus/stop/" + input +"/live.json?app_id=" + apiid + "&app_key=" + apikey + "&group=route&nextbuses=yes")
+//    	        .request(MediaType.TEXT_PLAIN)
+//    	        .get(String.class);
+//    	System.out.println(schedule);
+//    	String jsonResponse = schedule.readEntity(String.class);
+    	
+    	//
+    	
+		WebTarget webResource;
+		webResource = client.target("https://transportapi.com/v3/uk/bus/stop/" + input + "/live.json?app_id=" + apiid + "&app_key=" + apikey + "&group=route&nextbuses=yes");
+		String response = webResource.request(MediaType.TEXT_PLAIN).get(String.class);
+		System.out.println(response);
+		//String jsonResponse = response.
     	
     }
 }	
