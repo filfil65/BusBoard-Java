713364c910036d8f7eafbfbd0267268545a98d1b
BROWN Samuel <sabrown@europe.corp.altran.com>
2018-09-24 10:41:09 +0100

    Added API info in comments

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -3,6 +3,7 @@ package training.busboard;
 public class Main {
     public static void main(String args[]) {
         // Your code here!
-    	// Sam's edit.
+    	//Sam's API ID is: c3b16dc7
+    	// Sam's API key is : 5293d7854df1ddc814d674a109060164
     }
 }	
