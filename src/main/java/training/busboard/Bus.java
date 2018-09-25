d8a450ce9036f28da4df606bc6c0f298a6e35bdf
BROWN Samuel <sabrown@europe.corp.altran.com>
2018-09-25 09:55:49 +0100

    added extra bus functionality

--- src/main/java/training/busboard/Bus.java
+++ src/main/java/training/busboard/Bus.java
@@ -1,8 +1,13 @@
 package training.busboard;
 
 public class Bus {
-	String destination;
-	String line;
-	String plannedArrivalTime;
-	String eta;
+	String direction;
+	String line_name;
+	String best_departure_estimate;
+	String aimed_departure_time;
+	
+	public void getInfo()
+	{
+		System.out.println("The " + aimed_departure_time + " No. " + line_name + " bus to " + direction + ".\n    Expected at " + best_departure_estimate);
+	}
 }
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -7,43 +7,50 @@ public class Main {
 	public static void main(String args[]) {
 		// Sam's API ID is: c3b16dc7
 		// Sam's API key is : 5293d7854df1ddc814d674a109060164
+//
+//		Scanner readline = new Scanner(System.in);  
+//		System.out.println("Please enter a Post Code:");
+//		String input = readline.nextLine(); 
+//		readline.close();
+//
+//		PCAPI postCodeApi = new PCAPI();
+//		String jsonPostCodeData = postCodeApi.PC2JSON(input);
+////		System.out.println(jsonpostcodedata);
+//
+//		JSON2LL parserone = new JSON2LL(jsonPostCodeData);
+////		System.out.println(parserone);
+//		
+//		Place house = new Place();
+//		house.latitude = Double.toString(parserone.getLat());
+//		house.longitude = Double.toString(parserone.getLon());
+////		System.out.println("Lat is: " + house.latitude);
+////		System.out.println("Long is: " + house.longitude);
+//
+//		LLAPI llapi = new LLAPI();
+//		String jsonatcosdata = llapi.LL2JSON(house.latitude, house.longitude);
+//		System.out.println(jsonatcosdata);
+//
+//		//Parse jsonatcosdata to output a list of two ATCO codes (as a ArrayList<String>).
+//		ArrayList<String> atcos = new ArrayList<String>();
+//		//Parse jsonatcosdata to output a list of two ATCO codes (as a ArrayList<String>).
+//
+//		ArrayList<BusStop> busStops = new ArrayList<BusStop>();
+//		ATCOAPI atcoapi = new ATCOAPI();
+//		for (int i=0; i<2 && i<atcos.size(); i++)
+//		{
+//			String jsonbusstopdata = atcoapi.atco2BusStop(atcos.get(i));
+//			//do some more jsonparsing here.
+//			//store in a hashmap?
+//			BusStop outputfromtheparse = new BusStop();
+//			busStops.add(outputfromtheparse);
+//		}
 
-		Scanner readline = new Scanner(System.in);  
-		System.out.println("Please enter a Post Code:");
-		String input = readline.nextLine(); 
-		readline.close();
-
-		PCAPI pcapi = new PCAPI();
-		String jsonpostcodedata = pcapi.PC2JSON(input);
-		System.out.println(jsonpostcodedata);
-
-		JSON2LL parserone = new JSON2LL(jsonpostcodedata);
-		System.out.println(parserone);
+		Bus threethreeone = new Bus();
+		threethreeone.aimed_departure_time = "11.55";
+		threethreeone.best_departure_estimate = "12:08";
+		threethreeone.direction = "Hertford";
+		threethreeone.line_name = "331";
 		
-		Place house = new Place();
-		house.latitude = Double.toString(parserone.getLat());
-		house.longitude = Double.toString(parserone.getLon());
-		System.out.println("Lat is: " + house.latitude);
-		System.out.println("Long is: " + house.longitude);
-
-		LLAPI llapi = new LLAPI();
-		String jsonatcosdata = llapi.LL2JSON(lat, lon);
-		System.out.println(jsonatcosdata);
-
-		//Parse jsonatcosdata to output a list of ATCO codes (as a ArrayList<String>).
-		ArrayList<String> atcos = new ArrayList<String>();
-		//Parse jsonatcosdata to output a list of ATCO codes (as a ArrayList<String>).
-
-		ArrayList<BusStop> busStops = new ArrayList<BusStop>();
-		ATCOAPI atcoapi = new ATCOAPI();
-		for (int i=0; i<2; i++)
-		{
-			String jsonbusstopdata = atcoapi.atco2BusStop(atcos.get(i));
-			//do some more jsonparsing here.
-			//store in a hashmap?
-			BusStop outputfromtheparse = new BusStop();
-			busStops.add(outputfromtheparse);
-		}
-
+		threethreeone.getInfo();
 	}
 }	
