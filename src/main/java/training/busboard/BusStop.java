15309bc97f3c881dc002a7990f1393e05fc84ddc
BROWN Samuel <sabrown@europe.corp.altran.com>
2018-09-25 11:20:28 +0100

    fucking about with a new json

--- src/main/java/training/busboard/BusStop.java
+++ src/main/java/training/busboard/BusStop.java
@@ -1,11 +1,9 @@
 package training.busboard;
 
-import java.util.ArrayList;
-
 public class BusStop {
 	String atco;
 	String name;
 	String location;
 	int[] lines;
-	ArrayList<Bus> incomingBuses;
+	Bus[] incomingBuses;
 }
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -55,5 +55,10 @@ public class Main {
 //		threethreeone.line_name = "331";
 //		
 //		threethreeone.getInfo();
+		
+		JSON2BUS newtest = new JSON2BUS();
+		newtest.Json2Bus("{\"atcocode\":\"210021310420\",\"smscode\":\"hrtdpdpj\",\"request_time\":\"2018-09-25T11:00:17+01:00\",\"name\":\"White Horse PH\",\"stop_name\":\"White Horse PH\",\"bearing\":\"SW\",\"indicator\":\"o/s\",\"locality\":\"High Cross\",\"location\":{\"type\":\"Point\",\"coordinates\":[-0.02259,51.84905]},\"departures\":{\"331\":[{\"mode\":\"bus\",\"line\":\"331\",\"line_name\":\"331\",\"direction\":\"Hertford\",\"operator\":\"ARHE\",\"operator_name\":\"Arriva (in Herts and Essex)\",\"date\":\"2018-09-25\",\"aimed_departure_time\":\"11:21\",\"expected_departure_date\":null,\"expected_departure_time\":null,\"best_departure_estimate\":\"11:21\",\"dir\":\"inbound\",\"id\":\"https://transportapi.com/v3/uk/bus/route/ARHE/331/inbound/210021310420/2018-09-25/11:21/timetable.json?app_id=c3b16dc7\\u0026app_key=5293d7854df1ddc814d674a109060164\",\"source\":\"Traveline timetable (not a nextbuses live region)\"},{\"mode\":\"bus\",\"line\":\"331\",\"line_name\":\"331\",\"direction\":\"Hertford\",\"operator\":\"ARHE\",\"operator_name\":\"Arriva (in Herts and Essex)\",\"date\":\"2018-09-25\",\"aimed_departure_time\":\"12:21\",\"expected_departure_date\":null,\"expected_departure_time\":null,\"best_departure_estimate\":\"12:21\",\"dir\":\"inbound\",\"id\":\"https://transportapi.com/v3/uk/bus/route/ARHE/331/inbound/210021310420/2018-09-25/12:21/timetable.json?app_id=c3b16dc7\\u0026app_key=5293d7854df1ddc814d674a109060164\",\"source\":\"Traveline timetable (not a nextbuses live region)\"}]}}");
+		Bus bub =newtest.buses[0];
+		bub.getInfo();
 	}
 }	
