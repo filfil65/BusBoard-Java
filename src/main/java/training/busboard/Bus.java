52608650f112a10259f92df3ce9f547d731e7021
BROWN Samuel <sabrown@europe.corp.altran.com>
2018-09-26 15:59:47 +0100

    Time might be fixed now

--- src/main/java/training/busboard/Bus.java
+++ src/main/java/training/busboard/Bus.java
@@ -14,45 +14,41 @@ public class Bus {
 	public Long longTime;  //Time in milliseconds between bus' arrival and 1st Jan 1970.
 	SimpleDateFormat format = new SimpleDateFormat("HH:mm");
 
-	
+
 	public Bus(String direction, String line_name, String best_departure_estimate, String aimed_departure_time, String reqTime) throws ParseException
 	{
 		this.direction = direction;
 		this.line_name = line_name;
 		this.best_departure_estimate = best_departure_estimate;
 		this.aimed_departure_time = aimed_departure_time;
-		
+
 		Date date = format.parse(best_departure_estimate + ":00");
 		this.longTime = date.getTime();
-		
+
 		Pattern pattern = Pattern.compile(".*([0-9][0-9]:[0-9][0-9]:[0-9][0-9])\\+([0-9][0-9]:[0-9][0-9])");
 		Matcher matcher = pattern.matcher(reqTime);
 		matcher.find();
 		String formattedreqTime = matcher.group(1);
 		String modifier = matcher.group(2);
-		
+
 		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
 		Date date1 = format.parse(best_departure_estimate + ":00");
-		Date modifiedTime = format.parse(modifier + ":00");
-//		String fuck = new SimpleDateFormat("HH:mm:ss").format(formattedreqTime);
-		Date date2 = format.parse(formattedreqTime);
-		long difference = date1.getTime() - date2.getTime() - modifiedTime.getTime();
-		String output = (new SimpleDateFormat("HH:mm")).format(difference);
-		this.tta = output;
-		
+		if (modifier.equals("01:00")) {
+			Date date2 = format.parse(formattedreqTime);
+			long difference = date1.getTime() - date2.getTime() - 3600000;
+			String output = (new SimpleDateFormat("HH:mm")).format(difference);
+			this.tta = output;
+		}
+		else {
+			Date date2 = format.parse(formattedreqTime);
+			long difference = date1.getTime() - date2.getTime();
+			String output = (new SimpleDateFormat("HH:mm")).format(difference);
+			this.tta = output;
+
+		}
+
 	}
-	
-//	public void getTTA() throws ParseException
-//	{		
-//		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-//		Date date1 = format.parse(best_departure_estimate + ":00");
-//		String fuck = new SimpleDateFormat("HH:mm:ss").format(new Date());
-//		Date date2 = format.parse(fuck);
-//		long difference = date1.getTime() - date2.getTime();
-//		String output = (new SimpleDateFormat("mm")).format(difference);
-//		this.tta = output;
-//	}
-	
+
 	public void getInfo()
 	{
 		System.out.println(aimed_departure_time + " No. " + line_name + " bus to " + direction + ".\n    Expected at " + best_departure_estimate);
